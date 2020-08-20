c1e882a2bf7074777349841550511ec9a9aa0b2a
walid younis <walid.younis.90@gmail.com>
2020-08-20 15:17:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,9 +9,10 @@ end
 def summon_captain_planet(planeteer_calls)
 new_array = []
 planeteer_calls.map do |element|
-  "#{element.capitalize "!}"
+  "#{element.capitalize "!"}"
 end
-
+  new_array
+end 
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
